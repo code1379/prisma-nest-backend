4b1486a300d6a5f5e4c645de246ac5ba480e21b0
code1379 <coderxialuo@gmail.com>
2023-02-18 22:51:01 +0800

    feat: 使用 @nestjs/config 的ConfigModule 读取 .env文件 使用 cross-env 读取不同环境的配置文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+
+// 结合 cross-env 以及 ConfigModule 的 envFilePath 使用，但我觉得没必要。我们只用 .env 文件就行
+// const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      // 不设置 isGlobal 的话，在别的模块的 providers 中添加 ConfigService
+      isGlobal: true,
+      // envFilePath,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,15 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigEnum } from 'src/enum/config.enum';
 
 @Controller('user')
 export class UserController {
   // 使用 service 需要在 constructor 中定义一个变量
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {}
 
   @Get(':id')
   // @Query => /user?id=1&age=2
@@ -17,6 +22,16 @@ export class UserController {
     console.log(
       '🚀 ~ file: user.controller.ts:13 ~ UserController ~ getUsers ~ param',
       param,
+    );
+    const db = this.configService.get(ConfigEnum.DB);
+    const host = this.configService.get(ConfigEnum.DB_HOST);
+    console.log(
+      '🚀 ~ file: user.controller.ts:26 ~ UserController ~ getUsers ~ db',
+      db,
+    );
+    console.log(
+      '🚀 ~ file: user.controller.ts:27 ~ UserController ~ getUsers ~ host',
+      host,
     );
 
     return this.userService.getUsers();
