d556f19de228c3455c81fc2dc92e6600d4c8faa9
code1379 <coderxialuo@gmail.com>
2023-02-19 16:18:31 +0800

    feat: 所有异常的过滤器

---
+++ src/filters/all-exception.filter.ts
@@ -0,0 +1,48 @@
+/*
+  针对 http-exception 的 filter。
+  相当于当我们抛出 HttpException 的时候，会被捕获到
+*/
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import * as requestIp from 'request-ip';
+import { HttpAdapterHost } from '@nestjs/core';
+// 不添加参数的话，会捕获所有异常。包括 websocket 的
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  constructor(private httpAdapterHost: HttpAdapterHost) {}
+  catch(exception: unknown, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
+    // host 用于获取上下文（express 的）
+    const ctx = host.switchToHttp();
+    // 响应
+    const response = ctx.getResponse<Response>();
+    // 请求
+    const request = ctx.getRequest<Request>();
+    // 状态码
+    const statusCode =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+    // ip
+    const ip = requestIp.getClientIp(request);
+
+    const body = {
+      headers: request.headers,
+      query: request.query,
+      params: request.params,
+      body: request.body,
+      timestamp: new Date().toISOString(),
+      ip,
+      exception: exception['name'],
+      error: exception['response'] || 'INTERNAL_SERVER_ERROR',
+    };
+
+    httpAdapter.reply(response, body, statusCode);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { AllExceptionFilter } from './filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.setGlobalPrefix('/api/v1');
-  app.useGlobalFilters(new HttpExceptionFilter());
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapterHost));
+  // app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
